bb2c9ba5afda5dfbc0f7984213f7edd9d35a29cd
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-10-07 13:32:13 -0500

    working on tag routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -81,7 +81,7 @@ router.delete('/:id', async (req, res) => {
       res.status(400).json({ message: 'no category with that id'});
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json('category deleted');
   }catch (err) {
     res.status(500).json(err);
   }
